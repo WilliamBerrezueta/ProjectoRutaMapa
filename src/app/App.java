8587a12ecc68ff46f485c46d66064eb09e413b5b
WilliamBerrezueta <lolkio079@gmail.com>
2026-07-26 22:04:30 -0500

    App

--- src/app/App.java
+++ src/app/App.java
@@ -1,8 +1,22 @@
 package app;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import controllers.MapController;
+import views.MainFrame;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-    
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ignored) {
+                // se usa el look and feel por defecto si falla
+            }
+            MapController controller = new MapController();
+            MainFrame frame = new MainFrame(controller);
+            frame.setVisible(true);
+        });
+    } 
 }
